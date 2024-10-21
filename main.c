a11af2eba4296e5c6f3680cb78c246b5910f4a68
chenhao <chenhao.zhu@telecom-paris.fr>
2024-10-21 23:39:14 +0200

    section 2.4 Test with fibonacci function; The error expected didn't occur

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
+int global_var;
+
+int fibo(int n){
+	if ((n == 0)|(n == 1)) {
+		return 1;
+	}
+	else{
+		return fibo(n-1) + fibo(n-2);
+	}
+}
+
 
 int main(){
 	return fibo(8);
